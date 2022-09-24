aaa777bbb09a191f89c482974c4d1c162b15d6d4
Maksim Dudarev <maxim.d132213@gmail.com>
2022-09-24 11:50:52 +0300

    Base settings and create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import './App.css';
+import './index.css';
+import styles from './style';
+import { Navbar, Billing, Business, Button, CardDeal, Clients, CTA, FeedbackCard, Footer, GetStarted, Hero, Stats, Testimonials } from './components';
 
 const App = () => {
   return (
-    <div className="App">
-      <div style={{ textAlign: "center" }}> Hey, work in process...</div>
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={styles.boxWidth}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={styles.boxWidth}>
+          <Hero />
+        </div>
+      </div>
+      
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={styles.boxWidth}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+
     </div>
   );
 }
